bbfe4834bfa8b9555bb0c73287431e46cbe65016
DamolaAdewunmi <damolasd@gmail.com>
2019-05-31 11:40:30 +0100

    Mobile view style changes

--- src/components/StarWars.js
+++ src/components/StarWars.js
@@ -37,6 +37,7 @@ class App extends Component {
         return res.json();
       })
       .then(data => {
+        console.log(data);
         this.setState({ element: data.results });
       })
       .catch(err => {
@@ -47,7 +48,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="Header">React Wars</h1>
+        <h1 className="Header">Star Wars Characters</h1>
         <StarWarsCharacter
           characters={this.state.starwarsChars}
           />
